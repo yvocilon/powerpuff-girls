1f2ec4e1f9c92ae9c7393f0e9e893a523e0c20ff
Yvo Cilon <yvo.cilon@appcore.nl>
2020-02-04 14:48:15 +0100

    fetch episode on full reload

--- src/components/episode/Episode.tsx
+++ src/components/episode/Episode.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Container from "../container/Container";
 import TextImageHeader from "../text-image-header/TextImageHeader";
 import { selectEpisode } from "../../store/showsReducer";
+import { fetchShow } from "../../store/actions";
 
 const Episode = () => {
   const { id, episodeId } = useParams();
   const showId = parseInt(id as string, 10);
+  const dispatch = useDispatch();
 
   const episode = useSelector(
     selectEpisode(showId, parseInt(episodeId as string, 10))
   );
+
+  useEffect(() => {
+    if (!episode) {
+      dispatch(fetchShow(showId));
+    }
+  }, [episode]);
 
   if (!episode) {
     return <h1>Loading</h1>;
